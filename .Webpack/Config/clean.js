fa48d0f6b30b85976ade79b4da065fd423ceced3
kenilam <hello@ki-cl.com>
2019-04-13 01:46:46 -0700

    [FEAT] Upgraded several package versions

--- .Webpack/Config/clean.js
+++ .Webpack/Config/clean.js
@@ -3,13 +3,7 @@ import { path as root } from 'app-root-path'
 import CleanWebpackPlugin from 'clean-webpack-plugin'
 import { srcRoot as outputSrcRoot, tmpRoot } from './output'
 
-const cleaner = new CleanWebpackPlugin([tmpRoot, outputSrcRoot], {
-  allowExternal: false,
-  beforeEmit: false,
-  verbose: Args.verbose,
-  
-  root,
-})
+const cleaner = new CleanWebpackPlugin()
 
 export default {
   plugins: [cleaner],
