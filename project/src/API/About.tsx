7ee8582b7fc3af4a1449f21a42e65b932aead532
Keni Lam <hello@ki-cl.com>
2020-06-29 01:20:38 -0700

    [FIX] About Logo alignments [FIX] API About transitionType to SlideDown

--- project/src/API/About.tsx
+++ project/src/API/About.tsx
@@ -1,11 +1,12 @@
 import Spec from '@/API/spec';
 import { Asynchronizer } from '@/Components';
 import React, { FunctionComponent } from 'react';
+import { types } from '@/Components/CSSTransition/Type';
 
 const url = `${process.env.API_URL}/api/about`;
 
 const About: FunctionComponent<Spec.About.Props> = ({ children, ...rest }) => (
-  <Asynchronizer {...rest} awaitFor={url}>
+  <Asynchronizer {...rest} transitionType={types.SlideUp} awaitFor={url}>
     {children}
   </Asynchronizer>
 );
