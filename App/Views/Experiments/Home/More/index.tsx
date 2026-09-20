b4a8a13ba4de56d016c49bb4f30af14f809562fb
Keni Lam <admin@loci.world>
2026-09-20 16:36:56 -0700

    [fix] reduce More DOM complexity

--- App/Views/Experiments/Home/More/index.tsx
+++ App/Views/Experiments/Home/More/index.tsx
@@ -22,33 +22,27 @@ const COPY = {
 
 /** The closing screen, in normal flow after the stage, content centred. */
 const More: React.FunctionComponent = () => (
-  <section className={classNames(CLASS_NAME, 'kicl-position-relative')}>
-    <Layout alignContent='center' autoFlow='row' gap='none'>
-      <div className={`${CLASS_NAME}__body`}>
-        <Layout alignContent='center' autoFlow='row' gap='narrow'>
-          <div className={`${CLASS_NAME}__words`}>
-            <Heading
-              is='h2'
-              dense
-              className={classNames(
-                `${CLASS_NAME}__title`,
-                'kicl-text-transform-uppercase'
-              )}
-            >
-              {COPY.title}
-            </Heading>
-            <HyperLink
-              className={`${CLASS_NAME}__link`}
-              lookLikeButton
-              to={`/${EXPERIMENTS}`}
-            >
-              {COPY.bookmark}
-            </HyperLink>
-          </div>
-        </Layout>
-      </div>
+  <Layout alignContent='center' autoFlow='row' gap='narrow'>
+    <footer className={classNames(CLASS_NAME, `${CLASS_NAME}__body`, `${CLASS_NAME}__words`)}>
+      <Heading
+        is='h2'
+        dense
+        className={classNames(
+          `${CLASS_NAME}__title`,
+          'kicl-text-transform-uppercase'
+        )}
+      >
+        {COPY.title}
+      </Heading>
+      <HyperLink
+        className={`${CLASS_NAME}__link`}
+        lookLikeButton
+        to={`/${EXPERIMENTS}`}
+      >
+        {COPY.bookmark}
+      </HyperLink>
+    </footer>
     </Layout>
-  </section>
 );
 
 export { CLASS_NAME };
